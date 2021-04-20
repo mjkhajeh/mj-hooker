12d718fa1d1db4c826ed0117886cfbcf8a202159
mjfridge <fridge@mjkhajeh.com>
2021-04-20 14:03:10 +0430

    Translate theme/plugins header

--- Backend/Page.php
+++ Backend/Page.php
@@ -2,6 +2,7 @@
 namespace MJHooker\Backend;
 
 class Page {
+	private $active_tab = '';
 	public static function get_instance() {
 		static $instance = null;
 		if( $instance === null ) {
@@ -26,9 +27,48 @@ class Page {
 		);
 	}
 
+	private function tab_content_creator( $args ) {
+		?>
+		<div class="tab-content" id="mjhooker_tab_<?php echo $args['tab'] ?>"<?php echo $this->active_tab != $args['tab'] ? ' style="display:none"' : '' ?>>
+			<h2><?php echo $args['title'] ?></h2>
+			<div class="mjhooker_table_container">
+				<div class="mjhooker_overlay"></div>
+				<table class="mjhooker_table" id="mjhooker_plugins">
+					<thead>
+						<tr>
+							<th><?php _e( "Title", 'mjhooker' ) ?></th>
+							<th><?php _e( "Version", 'mjhooker' ) ?></th>
+							<th><?php _e( "Author", 'mjhooker' ) ?></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						foreach( $args['data'] as $data ) {
+							?>
+							<tr data-dir="<?php echo $data['dir'] ?>" data-type="<?php echo $args['data_type'] ?>">
+								<td>
+									<a href="#" class="mjhooker_dl"><?php echo $data['name'] ?></a>
+								</td>
+								<td><?php echo $data['version'] ?></td>
+								<td><?php echo $data['author_name'] ?></td>
+								<td>
+									<?php if( file_exists( "{$args['base_dir']}/{$data['dir']}.zip" ) ) { ?>
+										<a href="#" class="mjhooker_del"><i class="dashicons dashicons-trash"></i></a>
+									<?php } ?>
+								</td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<?php
+	}
+
 	public function view() {
-		$plugins = get_plugins();
-		$themes = wp_get_themes();
+		$plugins	= get_plugins();
+		$themes		= wp_get_themes();
 
 		$tabs = array();
 		if( !empty( $plugins ) ) {
@@ -37,10 +77,11 @@ class Page {
 		if( !empty( $themes ) ) {
 			$tabs['themes'] = __( "Themes", 'mjhooker' );
 		}
+
 		if( !empty( $tabs ) ) {
-			$active_tab = array_keys( $tabs )[0]; // Select first tab
+			$this->active_tab = array_keys( $tabs )[0]; // Select first tab
 			if( !empty( $_GET['tab'] ) ) {
-				$active_tab = sanitize_text_field( $_GET['tab'] );
+				$this->active_tab = sanitize_text_field( $_GET['tab'] );
 			}
 		}
 		?>
@@ -52,87 +93,52 @@ class Page {
 				if( !empty( $tabs ) ) {
 					foreach( $tabs as $tab => $title ) {
 						?>
-						<a href="#" class="nav-tab mjhooker_tab<?php echo $active_tab == $tab ? ' nav-tab-active' : '' ?>" data-tab="<?php echo $tab ?>"><?php echo $title ?></a>
+						<a href="#" class="nav-tab mjhooker_tab<?php echo $this->active_tab == $tab ? ' nav-tab-active' : '' ?>" data-tab="<?php echo $tab ?>"><?php echo $title ?></a>
 						<?php
 					}
 				}
 				?>
 			</nav>
-			<?php if( !empty( $plugins ) ) { ?>
-				<div class="tab-content" id="mjhooker_tab_plugins"<?php echo $active_tab != 'plugins' ? ' style="display:none"' : '' ?>>
-					<h2><?php _e( "Plugins", 'mjhooker' ) ?></h2>
-					<div class="mjhooker_table_container">
-						<div class="mjhooker_overlay"></div>
-						<table class="mjhooker_table" id="mjhooker_plugins">
-							<thead>
-								<tr>
-									<th><?php _e( "Title", 'mjhooker' ) ?></th>
-									<th><?php _e( "Version", 'mjhooker' ) ?></th>
-									<th><?php _e( "Author", 'mjhooker' ) ?></th>
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php
-								foreach( $plugins as $path => $plugin ) {
-									$path = dirname( $path );
-									?>
-									<tr data-dir="<?php echo $path ?>" data-type="plugin">
-										<td>
-											<a href="#" class="mjhooker_dl"><?php echo $plugin['Name'] ?></a>
-										</td>
-										<td><?php echo $plugin['Version'] ?></td>
-										<td><?php echo $plugin['AuthorName'] ?></td>
-										<td>
-											<?php if( file_exists( WP_PLUGIN_DIR . "/{$path}.zip" ) ) { ?>
-												<a href="#" class="mjhooker_del"><i class="dashicons dashicons-trash"></i></a>
-											<?php } ?>
-										</td>
-									</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			<?php } ?>
+			<?php
+			if( !empty( $plugins ) ) {
+				$plugins_data = array();
+				foreach( $plugins as $path => $plugin ) {
+					$plugin = get_plugin_data( WP_PLUGIN_DIR . "/{$path}" );
+					$plugins_data[] = array(
+						'dir'			=> dirname( $path ),
+						'name'			=> $plugin['Name'],
+						'version'		=> $plugin['Version'],
+						'author_name'	=> $plugin['AuthorName'],
+					);
+				}
+				$this->tab_content_creator( array(
+					'tab'		=> 'plugins',
+					'title'		=> __( 'Plugins', 'mjhooker' ),
+					'data_type'	=> 'plugins',
+					'base_dir'	=> WP_PLUGIN_DIR,
+					'data'		=> $plugins_data
+				) );
+			}
 
-			<?php if( !empty( $themes ) ) { ?>
-				<div class="tab-content" id="mjhooker_tab_themes"<?php echo $active_tab != 'themes' ? ' style="display:none"' : '' ?>>
-					<h2><?php _e( "Themes", 'mjhooker' ) ?></h2>
-					<div class="mjhooker_table_container">
-						<div class="mjhooker_overlay"></div>
-						<table class="mjhooker_table" id="mjhooker_themes">
-							<thead>
-								<tr>
-									<th><?php _e( "Title", 'mjhooker' ) ?></th>
-									<th><?php _e( "Version", 'mjhooker' ) ?></th>
-									<th><?php _e( "Author", 'mjhooker' ) ?></th>
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php
-								foreach( $themes as $theme ) {
-									$path = basename( $theme->get_stylesheet_directory() );
-									?>
-									<tr data-dir="<?php echo $path ?>" data-type="theme">
-										<td>
-											<a href="#" class="mjhooker_dl"><?php echo $theme->get( 'Name' ) ?></a>
-										</td>
-										<td><?php echo $theme->get( 'Version' ) ?></td>
-										<td><?php echo $theme->get( 'Author' ) ?></td>
-										<td>
-											<?php if( file_exists( WP_CONTENT_DIR . "/themes/{$path}.zip" ) ) { ?>
-												<a href="#" class="mjhooker_del"><i class="dashicons dashicons-trash"></i></a>
-											<?php } ?>
-										</td>
-									</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			<?php } ?>
+			if( !empty( $themes ) ) {
+				$themes_data = array();
+				foreach( $themes as $theme ) {
+					$themes_data[] = array(
+						'dir'			=> basename( $theme->get_stylesheet_directory() ),
+						'name'			=> __( $theme->get( 'Name' ), $theme->get( 'TextDomain' ) ),
+						'version'		=> __( $theme->get( 'Version' ), $theme->get( 'TextDomain' ) ),
+						'author_name'	=> __( $theme->get( 'Author' ), $theme->get( 'TextDomain' ) ),
+					);
+				}
+				$this->tab_content_creator( array(
+					'tab'		=> 'themes',
+					'title'		=> __( 'Themes', 'mjhooker' ),
+					'data_type'	=> 'themes',
+					'base_dir'	=> WP_CONTENT_DIR . "/themes",
+					'data'		=> $themes_data
+				) );
+			}
+			?>
 		</div>
 		<?php
 	}
